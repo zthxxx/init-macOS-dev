15220b75c17799ac770ec0a3affd2adf26a3ea69
zthxxx <zthxxx.me@gmail.com>
2020-08-01 01:13:00 +0800

    feat: update vimrc install

--- init-zsh.sh
+++ init-zsh.sh
@@ -7,5 +7,5 @@ sudo chsh -s `command -v zsh` $USER
 touch ~/.zshrc
 sudo chown $USER ~/.zshrc
 
-curl -sSL -H 'Cache-Control: no-cache' git.io/tvimrc -o ~/.vimrc
+curl -sSL -H 'Cache-Control: no-cache' https://github.com/zthxxx/TianVimrc/raw/master/install.sh | bash
 curl -sSL -H 'Cache-Control: no-cache' git.io/jovial | sudo bash -s $USER
