f14141a9e03021d187ab1843c6e035622b514815
zthxxx <zthxxx.me@gmail.com>
2023-08-30 22:17:00 +0800

    chore: clear app preference commands

--- init-app-preference.sh
+++ init-app-preference.sh
@@ -2,19 +2,64 @@
 
 set -ex
 
-# git user setting
+# Git Config
 git config --global user.name zthxxx
 git config --global user.email zthxxx.me@gmail.com
 git config --global core.ignorecase false
 git config --global core.quotepath false
 git config --global pull.ff only
 
-# screen config
+## https://github.com/banga/git-split-diffs
+## install `git-split-diffs` in 'init-application.sh'
+git config --global core.pager "git-split-diffs --color | less -RFX"
+git config --global split-diffs.theme-name dark
+
+## push the current branch with the same name on the remote
+## https://git-scm.com/docs/git-config#Documentation/git-config.txt-pushdefault
+git config --global push.default simple
+
+## Reuse Recorded Resolution in git-rebase
+## https://blog.gitbutler.com/git-tips-1-theres-a-git-config-for-that/#reuse-recorded-resolution
+git config --global rerere.enabled true
+git config --global rerere.autoUpdate true
+
+## https://blog.gitbutler.com/git-tips-2-new-stuff-in-git/#some-git-branch-stuff
+git config --global branch.sort -committerdate
+git config --global column.ui auto
+
+## https://blog.gitbutler.com/git-tips-2-new-stuff-in-git/#safe-force-pushing
+## always using `git push --force-with-lease` instead of `git push --force`
+git config --global alias.pushf push --force-with-lease
+
+## https://docs.gitlab.com/ee/user/project/repository/signed_commits/ssh.html#verify-commits-locally
+### if allowed_signers file not contains public key, append it
+if ! grep -qFf ~/.ssh/zthxxx.ed25519.pub ~/.ssh/allowed_signers 2>/dev/null; then
+  # format of "ALLOWED SIGNERS" section in `ssh-keygen`
+  echo "zthxxx.me@gmail.com $(cat ~/.ssh/zthxxx.ed25519.pub)" >> ~/.ssh/allowed_signers
+fi
+git config --global gpg.ssh.allowedSignersFile ~/.ssh/allowed_signers
+
+## https://blog.gitbutler.com/git-tips-1-theres-a-git-config-for-that/#conditional-configs
+mkdir -p ~/.config/git
+git config --file ~/.config/git/github user.name zthxxx
+git config --file ~/.config/git/github user.email zthxxx.me@gmail.com
+git config --file ~/.config/git/github user.signingKey ~/.ssh/zthxxx.ed25519
+git config --file ~/.config/git/github commit.gpgsign true
+git config --file ~/.config/git/github gpg.format ssh
+git config --global 'includeIf.hasconfig:remote.*.url:git@github.com:zthxxx/**.path' ~/.config/git/github
+
+
+# gpg for Git committer for all web commits made on GitHub.com
+curl -sS https://github.com/web-flow.gpg | gpg --import
+
+# screen & tmux config
 curl -sLH 'Cache-Control: no-cache' https://gist.github.com/zthxxx/df7e121048b12c80f260285d88e1091f/raw/.screenrc -o ~/.screenrc
 curl -sLH 'Cache-Control: no-cache' https://gist.github.com/zthxxx/bbbb37c100254d688236c21e8b977b85/raw/.tmux.conf -o ~/.tmux.conf
 
+
 # mackup setting: https://github.com/lra/mackup/blob/master/doc/README.md#icloud
 cp ./app-preferences/mackup.cfg  ~/.mackup.cfg
+
 
 # pip config
 mkdir -p ~/.pip/
@@ -26,18 +71,6 @@ mkdir -p ~/.aria2/
 cp ./app-preferences/aria2.conf  ~/.aria2/aria2.conf
 local bt_tracker=`curl -sSL -H 'Cache-Control: no-cache' https://github.com/ngosang/trackerslist/raw/master/trackers_all.txt | perl -0pe "s/\n+/,/gms"`
 perl -i -pe "s#^bt-tracker=.*#bt-tracker=${bt_tracker}#g" ~/.aria2/aria2.conf
-
-# vscode user preference
-mkdir -p ~/Library/Application\ Support/Code/User/
-cp ./app-preferences/vscode.json ~/Library/Application\ Support/Code/User/settings.json
-
-# hyperjs preference
-perl -i -pe "
-    s/^ \+fontSize:.*$/    fontSize: 16,/g;
-    s/^ \+cursorShape:.*$/    cursorShape: 'BEAM',/g;
-    s/^ \+shell:.*$/    shell: 'zsh',/g;
-    s/^ \+windowSize:.*$/    windowSize: [1420, 900],/g;
-" ~/.hyper.js
 
 
 # iTerm2 preference
@@ -54,6 +87,7 @@ defaults import com.googlecode.iterm2 ./app-preferences/iTerm2.plist
 ## iTerm2 plist 中已经包含所有 profiles
 # mkdir -p ~/Library/Application\ Support/iTerm2/DynamicProfiles
 # cp ./app-preferences/iTerm2-profile-Localhost.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/Localhost.json
+
 
 ## iTerm2 lrzsz setup
 # https://gist.github.com/zthxxx/9171c12538605d92781b74274ba8b9e1
